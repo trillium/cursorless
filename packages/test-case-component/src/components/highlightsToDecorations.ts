09d0fc14ca29206280bd16fab378cb81dee8acb3
Trillium Smith <trillium@trilliumsmith.com>
2026-01-21 11:08:01 -0800

    wip: Resolve layout shift in animated visualization

--- packages/test-case-component/src/components/highlightsToDecorations.ts
+++ packages/test-case-component/src/components/highlightsToDecorations.ts
@@ -31,10 +31,11 @@ function getStyleString(style: Style): string {
   );
   return (
     `background-color: ${style.backgroundColor};` +
-    `border-color: ${borderColor};` +
-    `border-style: ${getBorderStyle(style.borderStyle)};` +
+    `outline-color: ${borderColor};` +
+    `outline-style: ${getBorderStyle(style.borderStyle)};` +
     `border-radius: ${getBorderRadius(style.borderRadius)};` +
-    `border-width: ${BORDER_WIDTH};`
+    `outline-width: ${BORDER_WIDTH};` +
+    `outline-offset: -${BORDER_WIDTH};`
   );
 }
 
